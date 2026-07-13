d57bf5010ef8758165325175ea0805c9504a0974
Seyam <seyam.hossain.118833@gmail.com>
2026-07-13 13:06:50 +0600

    init: laravel practice project initialized

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Seyam Laravel Practice</title>
+</head>
+<body>
+    <h1>Welcome to Seyam Laravel Practice</h1>
+    <p>This is a simple welcome page for the Laravel practice application.</p>
+    <a href="#" class="btn">Go to Home</a>
+</body>
+</html>
