efed82d54af0134cfb11d8e9f6ce92a3f0f8835d
Seyam <seyam.hossain.118833@gmail.com>
2026-07-13 16:43:19 +0600

    style: update books index to use stacked card list design

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,11 +1,13 @@
 <x-layout title="Books">
-    <h1>Books</h1>
+    <h1 class="mb-6 text-2xl font-bold text-gray-900">Currently Available Books</h1>
 
-    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+    <div class="flex flex-col gap-4">
         @foreach ($books as $book)
             <x-card :href="route('books.show', $book['id'])">
-                <h2 class="text-lg font-semibold text-gray-900">{{ $book['title'] }}</h2>
-                <p class="text-sm text-gray-500">{{ $book['author'] }}</p>
+                <div>
+                    <h2 class="font-bold text-gray-900">{{ $book['title'] }}</h2>
+                    <p class="text-sm text-gray-500">{{ $book['author'] }}</p>
+                </div>
             </x-card>
         @endforeach
     </div>
