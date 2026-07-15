76741af9f25c876b6b449a9fda1b5b5ce26d1826
Seyam <seyam.hossain.118833@gmail.com>
2026-07-15 17:04:32 +0600

    chore: regenerate IDE helper model docblocks
    Reflects the author_name removal and current schema for
    Book, Author, User.

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -5,6 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $name
+ * @property string|null $bio
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Book> $books
+ * @property-read int|null $books_count
+ * @method static \Database\Factories\AuthorFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Author newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Author newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Author query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Author whereBio($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Author whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Author whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Author whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Author whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Author extends Model
 {
     /** @use HasFactory<\Database\Factories\AuthorFactory> */
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $title
+ * @property string $description
+ * @property numeric $price
+ * @property int $author_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Author $author
+ * @method static \Database\Factories\BookFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book whereAuthorId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Book extends Model
 {
     /** @use HasFactory<\Database\Factories\BookFactory> */
