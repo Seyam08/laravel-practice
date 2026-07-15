f112e9ae4edba5617a5ab64049d3060c27bc4ad2
Seyam <seyam.hossain.118833@gmail.com>
2026-07-15 17:23:34 +0600

    feat: add routes for book creation
    GET /add-book renders the form, POST /books handles submission.

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,7 @@ Route::get('/', function () {
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
 
 Route::get('/book/{id}', [BookController::class, 'show'])->name('books.show');
+
+Route::get('/add-book', [BookController::class, 'create'])->name('books.create');
+
+Route::post('/books', [BookController::class, 'store'])->name('books.store');
